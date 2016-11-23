b98e109828c62a83adcc603e226cf48830c8c70a
Julian Cesar dos Santos <julian.santos@serpro.gov.br>
2016-11-23 09:59:21 -0200

    Grande reestruturação de pacotes e nomes

--- backend/usuario/src/main/java/org/demoiselle/store/usuario/message/AppMessage.java
+++
@@ -1,8 +0,0 @@
-package org.demoiselle.store.usuario.message;
-
-import org.apache.deltaspike.core.api.message.MessageBundle;
-
-@MessageBundle
-public interface AppMessage {
-
-}
--- backend/venda/src/main/java/org/demoiselle/store/sale/message/AppMessage.java
+++
@@ -1,8 +0,0 @@
-package org.demoiselle.store.venda.message;
-
-import org.apache.deltaspike.core.api.message.MessageBundle;
-
-@MessageBundle
-public interface AppMessage {
-
-}
