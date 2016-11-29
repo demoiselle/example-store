9872cdd11914da9674bdb8399108e63e323c27d6
03066042995 <cassiomaes@gmail.com>
2016-11-29 17:04:08 -0200

    Ajustes...

--- backend/store-product/src/main/java/org/demoiselle/jee7/example/store/product/service/ProductREST.java
+++ backend/store-product/src/main/java/org/demoiselle/jee7/example/store/product/service/ProductREST.java
@@ -6,24 +6,23 @@
 package org.demoiselle.jee7.example.store.product.service;
 
 import io.swagger.annotations.Api;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import io.swagger.annotations.ApiOperation;
 
-import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.core.Response;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.websocket.server.PathParam;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import org.demoiselle.jee.core.api.security.SecurityContext;
 import org.demoiselle.jee.persistence.crud.AbstractREST;
+import org.demoiselle.jee.rest.annotation.ValidatePayload;
 import org.demoiselle.jee.security.annotation.LoggedIn;
 import org.demoiselle.jee7.example.store.product.entity.Product;
 
 /**
  *
- * @author 70744416353
- * @param <I>
  */
 @Api("Produto")
 @Path("product")
@@ -31,12 +30,35 @@ public class ProductREST extends AbstractREST<Product, Long > {
 
     @Inject
     private SecurityContext securityContext;
-
-  
-    @GET
-    @Path("testecom")
-    @LoggedIn
-    public Response testeCom() {
-        return Response.ok().entity(securityContext.getUser().toString()).build();
+       
+    @Override
+    @POST
+    @Transactional
+    @ValidatePayload
+    @ApiOperation(value = "Insere entidade no banco")
+    @LoggedIn    
+    public Product persist(Product entity) {
+        return bc.persist(entity);
     }
+    
+    @Override   
+    @PUT
+    @Transactional
+    @ValidatePayload
+    @LoggedIn    
+    @ApiOperation(value = "Atualiza a entidade", notes = "Atualiza")
+    public Product merge(Product entity) {
+        return bc.merge(entity);
+    }
+    
+    @Override   
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    @LoggedIn    
+    @ApiOperation(value = "Remove entidade")
+    public void remove(@PathParam("id") Long id) {
+        bc.remove(id);
+    }
+    
 }
