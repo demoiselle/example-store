593b9eb3386bb8e12ccb8755faa51424a79bea26
Leonardo Merlin <leonardo.merlin@gmail.com>
2016-12-14 16:53:56 -0200

    pequenos ajustes no menu lateral (sidebar)

--- frontend/src/app/layout/sidebar-menu/sidebar-menu.component.ts
+++ frontend/src/app/layout/sidebar-menu/sidebar-menu.component.ts
@@ -14,24 +14,22 @@ let template = require('./sidebar-menu.template.html');
 })
 
 export class SidebarMenuComponent {
-  public routes:any = routes;
-  public search:any = {};
-  public hash:string = '';
+  public routes: any = routes;
+  public search: any = {};
+  public hash: string = '';
 
-  public constructor(private router:Router, private authService: AuthService, private loginService: LoginService) {
+  public constructor(private router: Router, private authService: AuthService, private loginService: LoginService) {
     this.routes = this.routes.filter(
-      (v:any) => v.path !== '**' && v.data
-      
+      (v: any) => v.path !== '**' && v.data
+
     );
-    this.router.events.subscribe((event:any) => {
+    this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationEnd) {
         this.hash = event.url;
       }
     });
 
-    
-    //console.log(this.authService.isAuthorized(['ADMINISTRATOR']));
-
+    // console.log(this.authService.isAuthorized(['ADMINISTRATOR']));
   }
 
   onRouteSelected(route) {
