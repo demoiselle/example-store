21f7f1668994edfd21558828ea85f6ffe962d366
Marcelo Teixeira <marcelo.teixeira@serpro.gov.br>
2016-12-15 10:03:39 -0200

    Atualização do angular para v 2.3

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterContentInit, Component, ViewContainerRef } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Ng2BootstrapConfig, Ng2BootstrapTheme, ComponentsHelper } from 'ng2-bootstrap/ng2-bootstrap';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { AuthService } from '@demoiselle/security';
 
 
@@ -26,10 +27,16 @@ export class AppComponent implements AfterContentInit {
   public isBs3:boolean = Ng2BootstrapConfig.theme === Ng2BootstrapTheme.BS3;
   private viewContainerRef:ViewContainerRef;
 
-  public constructor(viewContainerRef:ViewContainerRef, componentsHelper:ComponentsHelper, private router:Router, private authService: AuthService) {
-    // You need this small hack in order to catch application root view container ref
-    this.viewContainerRef = viewContainerRef;
-    componentsHelper.setRootViewContainerRef(viewContainerRef);
+  public constructor(
+    viewContainerRef:ViewContainerRef,
+    componentsHelper:ComponentsHelper, 
+    private router:Router, 
+    private authService: AuthService,
+    public toastr: ToastsManager) {
+      // You need this small hack in order to catch application root view container ref (ng2-bootstrap)
+      this.viewContainerRef = viewContainerRef;
+      componentsHelper.setRootViewContainerRef(viewContainerRef);
+      this.toastr.setRootViewContainerRef(viewContainerRef); // hack for angular >= 2.2
   }
 
   public ngAfterContentInit():any {
