801fbebb0be2af9f9c985215ab5620db93a8e68a
Julian Cesar <juliancesar@gmail.com>
2017-01-09 16:34:40 -0200

    Exemplo de sobrescrita de Exception Treatment

--- backend/store-user/src/main/java/org/demoiselle/jee7/example/store/user/exception/ExceptionTreatmentApp.java
+++ backend/store-user/src/main/java/org/demoiselle/jee7/example/store/user/exception/ExceptionTreatmentApp.java
@@ -2,26 +2,21 @@ package org.demoiselle.jee7.example.store.user.exception;
 
 import java.util.logging.Logger;
 
-import javax.enterprise.inject.Alternative;
-import javax.inject.Inject;
+import javax.enterprise.inject.Specializes;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 
-import org.demoiselle.jee.core.exception.ExceptionTreatment;
 import org.demoiselle.jee.rest.exception.treatment.ExceptionTreatmentImpl;
 
-@Alternative
-public class ExceptionTreatmentApp implements ExceptionTreatment {
-
-	@Inject
-	private ExceptionTreatmentImpl exceptionTreatment;
+@Specializes
+public class ExceptionTreatmentApp extends ExceptionTreatmentImpl {
 
 	private static final Logger logger = Logger.getLogger(ExceptionTreatmentApp.class.getName());
 
 	@Override
 	public Response getFormatedError(Throwable exception, HttpServletRequest request) {
 		logger.info("Using ExceptionTreatmentApp");
-		return exceptionTreatment.getFormatedError(exception, request);
+		return super.getFormatedError(exception, request);
 	}
 
 }
